2d9d85e21a24440dfd411c8776ee6e2465562b94
imneil09 <iamneil03@gmail.com>
2020-04-09 02:29:40 +0530

    source re-designed

---
+++ lib/controller/data.dart
@@ -0,0 +1,32 @@
+class Data {
+  static const WEB_TITLE = "Neil's Outlet - Software Developer";
+  static const NAME = "Sagar Bhowmik";
+  static const PHONE_NUMBER = "+91-xxxx xxxxx3";
+  static const EMAIL = "iamneil03@gmail.com";
+  static const LOCATION = "Sonamura, Tripura";
+  static const BIRTHDAY = "xx xx, xxxx";
+
+  static const AVATAR = "image/avatar.jpg";
+
+  static const FACEBOOK_URL = "https://www.facebook.com/thanhnamitit";
+  static const INSTAGRAM_URL = "https://www.instagram.com/thanhnamitit/";
+  static const GITHUB_URL = "https://github.com/thanhnamitit";
+  static const LINKEDIN_URL = "https://www.linkedin.com/in/đỗ-nam-b51829167/";
+
+  static const HOME_SENTENCE =
+      "The proper use of comments is to compensate for our failure to express ourself in code.";
+  static const HOME_AUTHOR = "Robert C. Martin";
+
+  static const ABOUT_PAGE_INFO = {
+    "Name": NAME,
+    "Location": LOCATION,
+    "Birthday": BIRTHDAY,
+    "Email": EMAIL,
+  };
+
+  static const MESSAGE_API =
+      "https://us-central1-workout-e1b53.cloudfunctions.net/api/message";
+
+  static const CV_URL =
+      "https://www.topcv.vn/download-cv?cv_id=ee5ba82f207c20f1cc1d4f562df05f25&t=1586189535";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,4 @@
+import 'package:cv/app.dart';
 import 'package:flutter/material.dart';
-import 'config/themes.dart';
-import 'pages/home_page.dart';
-import 'widgets/theme_inherited_widget.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ThemeSwitcherWidget(
-      initialDarkModeOn: false,
-      child: NeilsOutlet(),
-    );
-  }
-}
-class NeilsOutlet extends StatelessWidget {
-  const NeilsOutlet({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Sagar Bhowmik',
-      theme:ThemeSwitcher.of(context).isDarkModeOn?darkTheme(context):lightTheme(context),
-      home: HomePage(),
-    );
-  }
-}
+void main() => runApp(App());
